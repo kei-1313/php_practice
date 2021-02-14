4ff329b7a3f1a846b3c324cdda9fe4944c665e61
kei-1313 <candydog666@gmail.com>
2021-02-14 22:47:13 +0900

    php_practice

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php echo "Good morning"."\n";?> 
+<?php echo "Good afternoon"."\n";?>
+<?php echo "Good evening"."\n";?>
+
+<?php 
+$num = 1;
+$num02 = 1.39;
+
+echo gettype($num);
+echo gettype($num02);
